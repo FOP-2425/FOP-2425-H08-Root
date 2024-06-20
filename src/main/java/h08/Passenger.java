330a44133ab6ecf8f901f04cb629c72977e10f9a
Ahmed C <ahmed.chouchane@stud.tu-darmstadt.de>
2024-06-20 13:16:59 +0200

    Implementation of Passenger class

---
+++ src/main/java/h08/Passenger.java
@@ -0,0 +1,22 @@
+package h08;
+
+public class Passenger {
+    private String passengerId;
+    private String name;
+
+
+    public Passenger(String passengerId, String name) {
+        this.passengerId = passengerId;
+        this.name = name;
+
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
