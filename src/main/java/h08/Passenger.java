cc2fb3937290c2a62b01a1abca3d1c9ddbedb2ea
Ahmed C <ahmed.chouchane@stud.tu-darmstadt.de>
2024-07-18 12:52:40 +0200

    Updated Passenger class

--- src/main/java/h08/Passenger.java
+++ src/main/java/h08/Passenger.java
@@ -1,22 +1,48 @@
 package h08;
 
+import java.time.LocalDate;
+
 public class Passenger {
-    private String passengerId;
-    private String name;
+    private String passengerID;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
 
-
-    public Passenger(String passengerId, String name) {
-        this.passengerId = passengerId;
-        this.name = name;
-
+    public Passenger(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.passengerID = generatePassengerID(firstName, lastName, dateOfBirth);
     }
 
-    public String getPassengerId() {
-        return passengerId;
+    private String generatePassengerID(String firstName, String lastName, LocalDate dateOfBirth) {
+        String initials = firstName.charAt(0) + String.valueOf(lastName.charAt(0));
+        return initials + dateOfBirth.hashCode();
     }
 
-    public String getName() {
-        return name;
+    public String getPassengerID() {
+        return passengerID;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+            "passengerID='" + passengerID + '\'' +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", dateOfBirth=" + dateOfBirth +
+            '}';
+    }
 }
