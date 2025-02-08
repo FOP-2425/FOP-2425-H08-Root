8dd06184271806bdabcdeed88271ad8f7cbda1ba
nyanyan <zentox@users.noreply.github.com>
2025-02-08 14:09:35 +0100

    22/32 done

--- src/graderPrivate/java/h08/H08_5_2_TestsPrivate.java
+++ src/graderPrivate/java/h08/H08_5_2_TestsPrivate.java
@@ -1,14 +1,16 @@
 package h08;
 
 import h08.assertions.Links;
+import h08.rubric.context.TestInformation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 import org.tudalgo.algoutils.tutor.general.annotation.SkipAfterFirstFailedTest;
-import org.tudalgo.algoutils.tutor.general.match.Matcher;
+import org.tudalgo.algoutils.tutor.general.assertions.Assertions2;
 import org.tudalgo.algoutils.tutor.general.reflections.FieldLink;
+import org.tudalgo.algoutils.tutor.general.reflections.MethodLink;
 import org.tudalgo.algoutils.tutor.general.reflections.TypeLink;
 
 /**
@@ -26,8 +28,9 @@ public class H08_5_2_TestsPrivate extends H08_Tests {
     void testManageFlightCode() {
         FlightManagement management = Mockito.mock(FlightManagement.class, Mockito.CALLS_REAL_METHODS);
         TypeLink airportLink = Links.getType(FlightManagement.class);
-        FieldLink airportsLink = airportLink.getField(Matcher.of(field -> field.name().equals("airports")));
-        FieldLink sizeLink = airportLink.getField(Matcher.of(field -> field.name().equals("size")));
+        MethodLink manageFlightLink = Links.getMethod(airportLink, "manageFlight", String.class, Flight.class, boolean.class);
+        FieldLink airportsLink = Links.getField(airportLink, "airports");
+        FieldLink sizeLink = Links.getField(airportLink, "size");
         Airport[] airports = new Airport[5];
         for (int i = 0; i < airports.length; i++) {
             airports[i] = Mockito.mock(Airport.class);
@@ -38,7 +41,18 @@ public class H08_5_2_TestsPrivate extends H08_Tests {
 
         Flight flight = Mockito.mock(Flight.class);
         Airport airport = airports[0];
-        management.manageFlight(airport.getAirportCode(), flight, true);
+        TestInformation info = TestInformation.builder()
+            .subject(manageFlightLink)
+            .input(builder -> builder
+                .add("airportCode", airport.getAirportCode())
+                .add("flight", flight)
+                .add("isAddOperation", true))
+            .build();
+        Assertions2.call(
+            () -> management.manageFlight(airport.getAirportCode(), flight, true),
+            info,
+            comment -> "Unknown exception occurred while managing flight."
+        );
         Mockito.verify(airport, Mockito.times(1)).addFlight(Mockito.eq(flight), Mockito.anyBoolean());
     }
 }
